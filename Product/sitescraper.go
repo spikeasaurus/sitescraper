327877daefc9d9cb3a9a6e76b032d54018b8cc0d
spikeasaurus <spikeasaurus.says.rawr@gmail.com>
2021-03-03 05:51:10 -0800

    Beginning

--- Product/sitescraper.go
+++ Product/sitescraper.go
@@ -3,26 +3,24 @@ package sitescraper
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"io"
 	"log"
-	"math/rand"
 	"net/http"
 )
 
 // Sitescraper ...
 func Sitescraper(w http.ResponseWriter, r *http.Request) {
 
-	//	fmt.Fprint(w, "Hi. I am your Google Assitant. Say Help for a list of things I can do.")
-
 	var d struct {
-		Message string `json:"message"`
+		URL             string `json:"url"`
+		RecursionDepth  string `json:"recursiondepth"`
+		MinimumFileSize string `json:"minfilesize"`
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
 		switch err {
 		case io.EOF:
-			fmt.Fprint(w, "Hello World!")
+			fmt.Fprint(w, "EOF")
 			return
 		default:
 			log.Printf("json.NewDecoder: %v", err)
@@ -31,16 +29,6 @@ func Sitescraper(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	phrases := []string{"I am not Alexa; I am your Google Assistant",
-		"Read me a bedtime story Home device not found",
-		"Turning off foyer lights",
-		"Confirming emergency 911 call",
-		"Restarting to install firmware"}
+	fmt.Fprint(w, "Url: "+d.URL+"\nRecursion Depth: "+d.RecursionDepth+"\nMinimum File Size: "+d.MinimumFileSize)
 
-	if d.Message != "" {
-		fmt.Fprint(w, phrases[rand.Intn(3)])
-		return
-	}
-
-	fmt.Fprint(w, html.EscapeString(d.Message))
 }
