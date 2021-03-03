b133882c34f11ef96e3acd0a377e41511460ee84
spikeasaurus <spikeasaurus.says.rawr@gmail.com>
2021-03-03 02:10:44 -0800

    asdf

--- Product/sitescraper.go
+++ Product/sitescraper.go
@@ -1,11 +1,36 @@
 package sitescraper
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"io"
+	"log"
+	"net/http"
+)
 
 // Sitescraper ...
-func Sitescraper() {
-	i := 1
-	i++
-	fmt.Println("i = ", i)
-	//wut happens
+func Sitescraper(w http.ResponseWriter, r *http.Request) {
+
+	var d struct {
+		Message string `json:"message"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		switch err {
+		case io.EOF:
+			fmt.Fprint(w, "Hello World!")
+			return
+		default:
+			log.Printf("json.NewDecoder: %v", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+	}
+
+	if d.Message == "" {
+		fmt.Fprint(w, "Hello World!")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
 }
