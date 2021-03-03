1781c55dd0ed9ee74fd635cb5e204154a9ba9b00
spikeasaurus <spikeasaurus.says.rawr@gmail.com>
2021-03-03 02:30:52 -0800

    jkl;

--- Product/sitescraper.go
+++ Product/sitescraper.go
@@ -6,11 +6,14 @@ import (
 	"html"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
 // Sitescraper ...
 func Sitescraper(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprint(w, "Hi. I am your Google Assitant. Say Help for a list of things I can do.")
 
 	var d struct {
 		Message string `json:"message"`
@@ -28,9 +31,16 @@ func Sitescraper(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
+	phrases := []string{"I am not Alexa; I am your Google Assistant",
+		"Read me a bedtime story Home device not found",
+		"Turning off foyer lights",
+		"Confirming emergency 911 call",
+		"Restarting to install firmware"}
+
+	if d.Message != "" {
+		fmt.Fprint(w, phrases[rand.Intn(4)])
 		return
 	}
+
 	fmt.Fprint(w, html.EscapeString(d.Message))
 }
