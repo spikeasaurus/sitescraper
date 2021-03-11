3fdc04f25d70440b965d595eccbd2faa01bc8b09
spikeasaurus <spikeasaurus.says.rawr@gmail.com>
2021-03-11 11:32:05 -0800

    test

--- Product/sitescraper.go
+++ Product/sitescraper.go
@@ -8,7 +8,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
+
+	//"net/url"
 	"regexp"
 	"strconv"
 	"strings"
@@ -221,12 +222,14 @@ func (j job) GetURIsFromPage(URI string, w *http.ResponseWriter, remainingDepth 
 		j.Debug(w, 2, "URI: ", URI, " >  n: ", n, " > remaining depth: ", remainingDepth, " > foundURI: ", ShortenText(foundURI, 125))
 
 		// Is the foundURI a relative URI or an absolute URI? If it's a relative URI, we should append the stem
-		var parentURI, relativeURI *url.URL
-		relativeURI, relativeURIError := relativeURI.Parse(foundURI)
-		j.Debug(w, 3, "relativeURI: ", ShortenText(relativeURI.String(), 125), "; error: ", relativeURIError)
-		parentURI, parentURIError := parentURI.Parse(URI)
-		j.Debug(w, 3, "parentURI: ", ShortenText(parentURI.String(), 125), "; error: ", parentURIError)
-		foundURI = parentURI.ResolveReference(relativeURI).String()
+		/*
+			var parentURI, relativeURI *url.URL
+			relativeURI, relativeURIError := relativeURI.Parse(foundURI)
+			j.Debug(w, 3, "relativeURI: ", ShortenText(relativeURI.String(), 125), "; error: ", relativeURIError)
+			parentURI, parentURIError := parentURI.Parse(URI)
+			j.Debug(w, 3, "parentURI: ", ShortenText(parentURI.String(), 125), "; error: ", parentURIError)
+			foundURI = parentURI.ResolveReference(relativeURI).String()
+		*/
 
 		// Did we process this already?
 		if checkedURIs[foundURI] == true {
